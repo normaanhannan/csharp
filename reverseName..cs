9428e8880c0e2db880d97f005abf074e0f520244
normaanhannan <70105237+normaanhannan@users.noreply.github.com>
2023-05-06 12:58:14 -0400

    Create reverseName..cs

---
+++ reverseName..cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+     string name = "normaan";
+
+
+
+        for(int i = name.Length -1; i>=0; i--)
+        {
+
+
+            Console.Write(name.ToCharArray()[i]);
+        }
+  
+  }
+}
